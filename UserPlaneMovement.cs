64aea83daed5f090d4b929b2fbeac85663e5bb70
dcurley <dcurley@mitre.org>
2023-02-01 01:40:55 -0500

    Relative hand position along y-axis rotates yaw of XR rig

--- UserPlaneMovement.cs
+++ UserPlaneMovement.cs
@@ -7,12 +7,22 @@ public class UserPlaneMovement : MonoBehaviour
 {
     bool accelerate_L, accelerate_R;
 
+    [SerializeField]
+    private Transform leftHand;
     private InputDevice l_Hand;
-
+    
+    [SerializeField]
+    private Transform rightHand;
     private InputDevice r_Hand;
-
+    
     [SerializeField]
     private GameObject userView;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField]
+    private int spin = 2;
 
     private List<InputDevice> devices = new List<InputDevice>();
 
@@ -30,6 +40,9 @@ public class UserPlaneMovement : MonoBehaviour
                 r_Hand = device;
             }
         }
+
+        //Get Rb component
+
     }
 
     // Update is called once per frame
@@ -39,13 +52,29 @@ public class UserPlaneMovement : MonoBehaviour
 
         r_Hand.TryGetFeatureValue(CommonUsages.gripButton, out accelerate_R);
 
-        if(accelerate_L || accelerate_R) {
-            Vector3 tempPos = userView.transform.position;
-            tempPos.z += .05f;
-            userView.transform.position = tempPos;
+
+        float tilt = leftHand.position.y - rightHand.position.y;
+        if(Mathf.Abs(tilt) > .1) {
+           userView.transform.Rotate(0.0f, tilt*spin, 0.0f, Space.Self);
         }
 
-        /*
+
+        
+
+        if(accelerate_L || accelerate_R) {
+            //Vector3 tempPos = userView.transform.position;
+            //tempPos.z += .05f;
+
+            //add force in this direction, not velocity change
+           
+            userView.transform.position += userView.transform.rotation * Vector3.forward * speed * Time.deltaTime;
+        
+            //Note: Camera view might not be the best option for direction
+            // We can use the camera offset, where how you hold your hands controls to orientation of the offset <- or XRRig
+            // Hands will rotate XR rig, 
+        }
+
+        /* //Maybe certain acceleration is needed to loop-de-loop?
         if(device.TryGetFeatureValue(gripUsage, out gripValue) && gripValue > 0 && !gripIsPressed) {
             gripIsPressed = true;
             DilmerGamesLogger.Instance.LogInfo($"Grip value {gripValue} activated on {xRNode}");
