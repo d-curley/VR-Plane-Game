1d9d4163751f3314a6ce8306673c60689dbaf6ee
dcurley <dcurley@mitre.org>
2023-02-08 03:31:13 -0500

    Features implements: -Access to controllers when disconnected -System of 3 equations to solve for controller.z rotation to desired pitch control -Pitch control -Tweak speed amplifiers for feel

--- UserPlaneMovement.cs
+++ UserPlaneMovement.cs
@@ -1,11 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
 public class UserPlaneMovement : MonoBehaviour
 {
-    bool accelerate_L, accelerate_R;
+    private bool accelerate_L, accelerate_R;
+    private bool pitch_L, pitch_R;
+    private Quaternion rotation_L, rotation_R;
 
     [SerializeField]
     private Transform leftHand;
@@ -19,55 +21,85 @@ public class UserPlaneMovement : MonoBehaviour
     private GameObject userView;
 
     [SerializeField]
-    private float speed = 1f;
+    private float thrust = 1.4f;
 
     [SerializeField]
-    private int spin = 2;
+    private float yawSpeed = 10;
+
+    [SerializeField]
+    private float pitchSpeed = 10;
 
     private List<InputDevice> devices = new List<InputDevice>();
 
     // Start is called before the first frame update
     void Start()
     {
+
+
+        refreshDevices();
+
+        //Get Rb component
+
+    }
+
+    void refreshDevices() {
         var inputDevices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-
         foreach(var device in inputDevices) {
-            if(device.role== InputDeviceRole.LeftHanded) {
+            if(device.role == InputDeviceRole.LeftHanded) {
                 l_Hand = device;
             }
             if(device.role == InputDeviceRole.RightHanded) {
                 r_Hand = device;
             }
         }
-
-        //Get Rb component
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        l_Hand.TryGetFeatureValue(CommonUsages.gripButton, out accelerate_L);
-
-        r_Hand.TryGetFeatureValue(CommonUsages.gripButton, out accelerate_R);
-
-
-        float tilt = leftHand.position.y - rightHand.position.y;
-        if(Mathf.Abs(tilt) > .1) {
-           userView.transform.Rotate(0.0f, tilt*spin, 0.0f, Space.Self);
+        if(!l_Hand.isValid || !r_Hand.isValid) {
+            refreshDevices();
         }
 
+        float yaw = leftHand.position.y - rightHand.position.y;
+        if(Mathf.Abs(yaw) > .1) {
+            userView.transform.Rotate(0.0f, yaw * yawSpeed* Time.deltaTime, 0.0f, Space.Self);
+        }
 
-        
+        l_Hand.TryGetFeatureValue(CommonUsages.primaryButton, out pitch_L);
+        r_Hand.TryGetFeatureValue(CommonUsages.primaryButton, out pitch_R);
+        if(pitch_L && pitch_R) {
 
+            float lPitch= leftHand.rotation.eulerAngles.z;
+            //float rPitch = 360 - rightHand.rotation.eulerAngles.z;
+            float pitchAVG = lPitch;// + rPitch); / 2;
+            Debug.Log("avg: " + pitchAVG);
+            /*
+            if(345 < pitchAVG || pitchAVG < 20f) {
+                pitchAVG = 20f;
+            }
+            if(310f < pitchAVG && pitchAVG < 345f) {
+                pitchAVG = 310f;
+            }*/
+            float pitchControl = (pitchAVG * pitchAVG * 0.000065f - pitchAVG * 0.02475f + 1.49f) *1.3f;//a= and b=-0.024746 and c=1.4691
+            pitchControl = Mathf.Clamp(pitchControl, -1, 1);
+            
+            Debug.Log("Final: "+pitchControl);
+
+            userView.transform.Rotate(-(pitchControl * Time.deltaTime * pitchSpeed),0.0f, 0.0f, Space.Self);
+            //userView.transform.RotateAround(userView.transform.position, userView.transform.localPosition.x, pitchControl*Time.deltaTime*-10f);
+        } 
+
+        l_Hand.TryGetFeatureValue(CommonUsages.gripButton, out accelerate_L);
+        r_Hand.TryGetFeatureValue(CommonUsages.gripButton, out accelerate_R);
         if(accelerate_L || accelerate_R) {
             //Vector3 tempPos = userView.transform.position;
             //tempPos.z += .05f;
 
             //add force in this direction, not velocity change
            
-            userView.transform.position += userView.transform.rotation * Vector3.forward * speed * Time.deltaTime;
+            userView.transform.position += userView.transform.rotation * Vector3.forward * thrust * Time.deltaTime;
         
             //Note: Camera view might not be the best option for direction
             // We can use the camera offset, where how you hold your hands controls to orientation of the offset <- or XRRig
